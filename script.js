2590cf0501c2a5a0e9896c8e0a69915d3b2cd6ea
kolawole-ysf <111007555+kolawole-ysf@users.noreply.github.com>
2022-12-13 17:19:17 +0000

    New Changes to Ui

---
+++ script.js
@@ -0,0 +1,17 @@
+
+//Animation scroll 
+const observer=new IntersectionObserver(entries=>{
+        entries.forEach(entry=>{
+                if(entry.isIntersecting){
+                        entry.target.classList.add('scroll-anim')
+                }else{
+                        entry.target.classList.remove('scroll-anim')
+                }
+        })
+});
+
+const hiddenElements=document.querySelectorAll('#hidden')
+hiddenElements.forEach(element=>{
+        observer.observe(element);
+})
+
